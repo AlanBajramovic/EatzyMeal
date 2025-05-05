e3134582a929e7d7d3bfdafc578cbf3e2d4207c6
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2025-05-05 23:14:10 +0200

    javascript till knapparna i foodgalleri så att man kan ta sig till respektive mat meny

---
+++ FoodGalleri/FoodGScript.js
@@ -0,0 +1,23 @@
+function goToSushi() {
+    window.location.href = 'menus/sushi/sushimenu.html';
+  }
+
+function goToHamburger() {
+    window.location.href = 'menus/hamburger/hamburgermenu.html';
+  }
+
+function goToPizza() {
+    window.location.href = 'menus/pizza/pizzamenu.html';
+  }
+
+function goToNoodle() {
+    window.location.href = 'menus/noodle/noodlemenu.html';
+  }
+
+function goToGrill() {
+    window.location.href = 'menus/grill/grillmenu.html';
+  }
+
+function goToSpagetthi() {
+    window.location.href = 'menus/spagetthi/spagetmenu.html';
+  }
