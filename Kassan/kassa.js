225da912ed1d097ca980bae78ae5f1f3231c7e7c
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2025-05-13 09:46:22 +0200

    mer till kassan

--- Kassan/kassa.js
+++ Kassan/kassa.js
@@ -5,3 +5,13 @@ function goToGalleri(){
 function buy(){
     alert("Din beställning har gått igenom")
   }
+
+  const buttonGroup = document.getElementById('paymentButtons');
+  const buttons = buttonGroup.querySelectorAll('.btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('active')); // remove active from all
+      button.classList.add('active'); // add to clicked one
+    });
+  });
