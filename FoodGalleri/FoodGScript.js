9358f13400555f5f6a55eb647cc125c44326764d
Vel1ko <veljko.jovic@elev.ga.ntig.se>
2025-05-15 08:56:27 +0200

    popup icon vid add to cart knappen

--- FoodGalleri/FoodGScript.js
+++ FoodGalleri/FoodGScript.js
@@ -29,3 +29,23 @@ function goToGalleri(){
 function goToKassa(){
   window.location.href = '../../../../kassan/kassa.html'
 }
+
+document.querySelectorAll('.addToCart').forEach(button => {
+  button.addEventListener('click', (e) => {
+    const popup = document.createElement('div');
+    popup.classList.add('check-popup');
+    popup.innerHTML = '+1';
+
+    // Positionen vid knappen
+    const rect = button.getBoundingClientRect();
+    popup.style.left = `${rect.left + rect.width / 2}px`;
+    popup.style.top = `${rect.top - 20 + window.scrollY}px`; // flyttas up
+
+    document.body.appendChild(popup);
+
+    // animation till att det försvinner
+    setTimeout(() => {
+      popup.remove();
+    }, 1000);
+  });
+});
